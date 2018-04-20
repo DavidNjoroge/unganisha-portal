c3b875bc848a6d829eaa04f48632e9a2b01b549d
KarlGusta <annoh.karlgusta@students.jkuat.ac.ke>
2018-04-20 18:39:37 +0300

    For the react part

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'react-tilt';
 import './App.css';
+import Logo from './Components/Logo/Logo';
+import Navigation from './Components/Navigation/Navigation';
+import WelcomeText from './Components/WelcomeText/WelcomeText';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navigation/>
+        <Logo/>
+        <WelcomeText/>
+       {/* <Maps/>
+        {/*<Users/>
+        <Requestings/>*/}
       </div>
     );
   }
---
+++ src/Components/WelcomeText/WelcomeText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomeText = () => {
+  return(
+    <div >
+      <h1>Welcome to Unganishi App</h1>
+      <h2>Kindly search your location</h2>
+    </div>
+    );
+}
+
+export default WelcomeText;
